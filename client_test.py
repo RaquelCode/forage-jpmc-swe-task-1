7c7691a2552189072b6c76c6c30d6ab9dab2f915
Revature <rqlmoura@yahoo.com>
2023-05-12 19:24:55 -0700

    Final update Clinte3.py

--- client_test.py
+++ client_test.py
@@ -2,6 +2,20 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
+  from client3 import getDataPoint
+  def test_getDataPoint(self):
+    quote = {
+      'stock': 'ABC',
+      'top_bid': {'price': '10.0'},
+      'top_ask': {'price': '12.0'}
+    }
+    expected_data_point = ('ABC', 10.0, 12.0, 11.0)
+
+    actual_data_point = getDataPoint(quote)
+
+    self.assertEqual(actual_data_point, expected_data_point)
+
+
   def test_getDataPoint_calculatePrice(self):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -17,6 +31,8 @@ class ClientTest(unittest.TestCase):
     ]
     for quote in quotes:
       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+
+
 
 
 
